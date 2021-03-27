ee20ccce98137668543463b6eb1ab9ef917ae8c3
master@eleven.net.cn <jun.xi@ximalaya.com>
2021-03-27 17:07:50 +0800

    fix: class name

--- packages/components/src/Foo/index.tsx
+++ packages/components/src/Foo/index.tsx
@@ -1,7 +1,7 @@
 import React, { FC } from 'react';
 
 const Foo: FC = () => {
-  return <div class="e-fe_foo-wrapper">foo...</div>
+  return <div className="e-fe_foo-wrapper">foo...</div>
 }
 
 export default Foo;
