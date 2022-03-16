72be07fe94dda431f410da5d03abe40d1d2f55cc
Eleven <jun.xi@ximalaya.com>
2022-03-16 10:39:58 +0800

    perf: update compareVersion
    affects: @e.fe/utils

--- packages/utils/src/compareVersion/index.ts
+++ packages/utils/src/compareVersion/index.ts
@@ -2,21 +2,22 @@ type CompareResult = 1 | -1 | 0;
 
 /**
  * 比较版本号
+ *  仅支持 x.x.x 普通版本号比较，不支持 alpha、beta 版本号
  *  - v1 > v2 返回 1
  *  - v1 === v2 返回 0
  *  - v1 < v2 返回 -1
  *
  * @param v1 版本号 1
  * @param v2  版本号 2
- * @param seprator 分隔符，默认：.
+ * @param separator 分隔符，默认：.
  */
 export default function compareVersion(
   v1: string,
   v2: string,
-  seprator = '.',
+  separator = '.',
 ): CompareResult {
-  const arr1 = v1.split(seprator);
-  const arr2 = v2.split(seprator);
+  const arr1 = v1.split(separator);
+  const arr2 = v2.split(separator);
   let result: CompareResult = 0;
   let i = 0;
 
